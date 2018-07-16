534fbbbb8a27d567d306417bd088a83e375d6463
Michael Prather <mprather1@gmail.com>
2018-07-16 06:19:43 +0000

    whoops404

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom'
 import Navbar from './Navbar'
 import Home from '../components/Home'
 import DeviceList from '../components/DeviceList'
+import Whoops404 from '../components/Whoops404'
 require('../../assets/less/index.less')
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
             <Switch>
               <Route exact path='/' component={Home} />
               <Route exact path='/devices' component={DeviceList} />
+              <Route component={Whoops404} />
             </Switch>
           </div>
         )} />
