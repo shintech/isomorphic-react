f6cb78f65983eb3ec4f7584b79a2eb508212e755
Michael Prather <mprather1@gmail.com>
2018-07-16 07:05:45 +0000

    listening message

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const React = require('react')
 const ignoreStyles = require('ignore-styles') // eslint-disable-line
 const app = require('./dist/server')
+const pkg = require('./package.json')
+const chalk = require('chalk')
 
 global.React = React
 
@@ -8,6 +10,8 @@ app().then(server => {
   server.set('port', process.env.PORT || 8000)
     .listen(
       server.get('port'),
-      () => console.log('listening...')
+      () => {
+        console.log(`${chalk.bgBlack.cyan(pkg.name)} version ${chalk.bgBlack.yellow(pkg.version)} -> listening on port ${chalk.bgBlack.green(server.get('port'))}...`)
+      }
     )
 })
